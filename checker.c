b0a4bcb94e23c6abb8d65ce2ea50356fcc79e7da
kumrdpk123 <kumrdpk123@gmail.com>
2024-09-02 18:00:03 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,32 +3,21 @@
 
 
 int temp_check(float temperature){
-if(temperature >0 && temperature < 45) 
+if(temperature >=0 && temperature < 45) 
     printf("Temperature in the range!\n");
-    else
-    printf("Temperature out of range!\n");
-    return 0;
 	}
 	
 int soc_Check(float soc){
   if(soc > 20 && soc < 80) 
      printf("State of Charge in range!\n");
-      else
-    printf("State of Charge out of range!\n");
-    return 0;
 	 
-  
-}
+  }
 
 
 
 int check_chargeRate(float chargeRate) {
-if(chargeRate < 0.8) 
-    printf("Charge Rate in of range!\n");
-     else
-    printf("Charge Rate out of range!\n");
+return (chargeRate < 0.8) ;
     
-    return 0;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
