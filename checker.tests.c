bd9aa6081aab7929753a49215121c8a8360cac1d
kumrdpk123 <kumrdpk123@gmail.com>
2024-11-05 21:29:54 +0530

    Update warning_system.c

--- checker.tests.c
+++ checker.tests.c
@@ -1,7 +1,27 @@
-#include "checker.h"
+#include <stdio.h>
+#include "warning_system.h"
 
-void  givemValidBatteryParemeterValuesTrueIsExpected(){
-  int expectedValue=1;
-  int actualValue=batteryIsOk(25,10,0.6) ;
-  assert(.....);
+float tolerance = 0.05;  // 5% tolerance
+
+void printWarningMessage(const char* message) 
+{
+    if (message[0] != '\0') {
+        printf("%s", message);
+    }
+}
+
+void checkLowThreshold(float value, float min, float max, const char* lowWarningMessage) 
+{
+    float lowerWarningLimit = min + (tolerance * max);
+    if (value > min && value <= lowerWarningLimit) {
+        printWarningMessage(lowWarningMessage);
+    }
+}
+
+void checkHighThreshold(float value, float max, const char* highWarningMessage) 
+{
+    float upperWarningLimit = max - (tolerance * max);
+    if (value >= upperWarningLimit && value < max) {
+        printWarningMessage(highWarningMessage);
+    }
 }
